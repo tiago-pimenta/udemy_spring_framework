0b716285da9036405fc33a6b015af71d54486e65
tiago-pimenta <tiagopimenta@gmail.com>
2024-03-31 00:51:16 -0300

    blah

---
+++ src/main/java/br/com/tiagopimenta/spring5tutorial/NpSpring5TutorialApplication.java
@@ -0,0 +1,13 @@
+package br.com.tiagopimenta.spring5tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpSpring5TutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpSpring5TutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tiagopimenta/spring5tutorial/NpSpring5TutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tiagopimenta.spring5tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NpSpring5TutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
